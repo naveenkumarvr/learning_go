a9fa05fc8da1567686e707f4238c94c512f100b0
Naveenkumar VR <naveenkumar.vasudevananthrajalakshmi@philips.com>
2024-09-09 10:49:10 +0530

    01 Getting started

---
+++ 01-getting_started/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+// this is a comment
+
+func main(){
+	// Go will by default call them main function. All executable function should have main function
+	fmt.Println("Hello World!")
+}
