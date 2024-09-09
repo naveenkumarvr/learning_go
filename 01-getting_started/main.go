73b331c924a6bca2bdaa48ee2206d22c37ad1705
Naveenkumar VR <naveenkumar.vasudevananthrajalakshmi@philips.com>
2024-09-09 10:50:31 +0530

    01 Getting started

--- 01-getting_started/main.go
+++ 01-getting_started/main.go
@@ -8,3 +8,7 @@ func main(){
 	// Go will by default call them main function. All executable function should have main function
 	fmt.Println("Hello World!")
 }
+
+/* This is the multi line comment
+where we can add multiple line
+*/
