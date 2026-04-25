72df2d68681226ed86607ea76045787cac54da3e
Naveenkumar VR <naveenkumar.vasudevananthrajalakshmi@philips.com>
2026-04-25 16:15:01 +0530

    Control flow learned

--- gemini-course/module-1/validator.go
+++ gemini-course/module-1/validator.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	var dbHost string = "localhost"
-	var dbPort int = 67700
+	var dbPort int = 5432
 	var isSSL bool = false
 
 	fmt.Printf("The Db Host %s dbPort is %d and is bool %t", dbHost, dbPort, isSSL)
@@ -15,14 +15,27 @@ func main() {
 	for _, server := range clusterHost {
 		dbHost = server
 		fmt.Printf("Checking %s", dbHost)
-		if dbHost == "" {
-			fmt.Printf("\nERROR: Host is empty")
+		// if len(dbHost) == 0 {
+		// 	fmt.Printf("\nERROR: Host is empty")
+		// }
+
+		// Improvised Condtion
+		if hLen := len(dbHost); hLen == 0 {
+			fmt.Printf("\n Error: Host is Empyt")
 		}
+
 		if dbPort > 65535 || dbPort < 1 {
 			fmt.Printf("\nERROR: Invalid port")
+		} else if dbPort < 1024 {
+			fmt.Printf("\n WARNING: Privileged port")
+		} else {
+			fmt.Printf("\n SUCCESS: Standard Port")
 		}
+
 		if !isSSL {
 			fmt.Printf("\nWARNING: Connecting over an insecure connection")
+		} else {
+			fmt.Printf("\n [SECURE]: Connection is encrypted")
 		}
 	}
 
---
+++ gemini-course/module-1/validator_switch.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	hostPort := map[string]int{
+		"prod-web-01": 80,
+		"prod-db-01":  5432,
+		"bad-server":  99999,
+	}
+
+	for dbHost, port := range hostPort {
+		switch {
+		case port > 65534 || port < 1:
+			fmt.Printf("\n ERROR: Invalid port %s, %d", dbHost, port)
+		case port < 1024:
+			fmt.Printf("\n WARN: Privileged Port %s, %d", dbHost, port)
+		default:
+			fmt.Printf("\nSUCCESS: Standard port %s, %d", dbHost, port)
+		}
+	}
+}
