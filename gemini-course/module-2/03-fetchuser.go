c416becdb4cdca97c214d93896298fb879b774dd
Naveenkumar VR <naveenkumar.vasudevananthrajalakshmi@philips.com>
2026-07-04 10:50:22 +0530

    Fetchuser task completed

--- gemini-course/module-2/03-fetchuser.go
+++ gemini-course/module-2/03-fetchuser.go
@@ -2,30 +2,52 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
 )
 
 type Slideshow struct {
-	Title  string `json: "title"`
-	Author string `json: "author"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
 }
 
 type ApiResponse struct {
-	Slideshow Slideshow `json: "slideshow"`
+	Slideshow Slideshow `json:"slideshow"`
 }
 
 func fetchUserActivity() {
 
 	// Adding Context
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
 	// Gate 1
-	req, err := http.NewReqeustWithContext(ctx, "GET", "https://httpbin.org/delay/1", nil)
+	req, err := http.NewRequestWithContext(ctx, "GET", "https://httpbin.org/json", nil)
 	if err != nil {
 		fmt.Printf("Error Creating Request: %v\n", err)
 		return
 	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Printf("Connection error %v", err)
+		return
+	}
+	defer resp.Body.Close()
+	var apiResp ApiResponse
+	if err := json.NewDecoder(resp.Body).Decode(&apiResp); err != nil {
+		fmt.Printf("Error decoding data \n %v", err)
+		return
+	}
+	fmt.Printf("Decoding Successful")
+	fmt.Printf("%v\n", apiResp)
+	fmt.Printf("Title: %s\n", apiResp.Slideshow.Title)
+	fmt.Printf("Author: %s\n", apiResp.Slideshow.Author)
+
+}
+
+func main() {
+	fetchUserActivity()
 }
